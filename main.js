a6ebac58a000abf5429a7844914552e1372722e5
Rob <robert@elddir.io>
2021-07-20 17:27:27 -0400

    add port to listen on

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const app = express();
     app.get('/', () => {
         res.send('Server is up and running');
     });
+
+app.listen(3000, (req, res) => {
+    console.log('server listening on port 3000')
+});    
