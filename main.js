c993a4d41c88633bebe707a4ba7e40a8cc2a475f
Rob <robert@elddir.io>
2021-07-20 17:54:36 -0400

    add html ref

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const express = require('express');
 
 const app = express();
     app.get('/', () => {
-        res.send('Server is up and running');
+        res.sendFile(__dirname + "/index.html");
     });
 
 app.listen(3000, (req, res) => {
     console.log('server listening on port 3000')
 });    
+
