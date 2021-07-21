11e7673797626dd0d9076d153fd7e7489fd90288
Rob <robert@elddir.io>
2021-07-21 13:14:10 -0400

    add env file deleted config module, add smartsheet option

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
+const client = require('smartsheet');
+require('dotenv').config();
 
 const app = express();
-    app.get('/', () => {
-        res.sendFile(__dirname + "/index.html");
-    });
+
+const smartsheet = client.createClient({
+    accessToken: process.env.ACCESS_TOKEN,
+    logLevel: "info",
+  });
+
+  var options = {
+    id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
+  };
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(3000, (req, res) => {
     console.log('server listening on port 3000')
