1aa87c6c6c422f19e6fad30355cb9327346e93f9
Rob <robert@elddir.io>
2021-07-21 17:00:30 -0400

    built code to add a row in the smartsheet and test passed. add routes and HTML successful post with "hello there" message

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const client = require('smartsheet');
 require('dotenv').config();
-
 const app = express();
 
-const smartsheet = client.createClient({
-    accessToken: process.env.ACCESS_TOKEN,
-    logLevel: "info",
-  });
-
-  var options = {
-    id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
-  };
-
+app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
@@ -21,5 +12,54 @@ app.get('/', (req, res) => {
 
 app.listen(3000, (req, res) => {
     console.log('server listening on port 3000')
-});    
+});
+
+// const smartsheet = client.createClient({
+//     accessToken: process.env.ACCESS_TOKEN,
+//     logLevel: "info",
+//   });
+
+//   var options = {
+//     id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
+//   };
+
+// // Get sheet
+// smartsheet.sheets
+//   .getSheet(options)
+//   .then(function (sheetInfo) {
+//     let myColumn = sheetInfo.columns.find((c) => c.title === "Project Name");
+//     let myColumnId = myColumn.id;
+//     // Specify rows
+//     var rows = [
+//       {
+//         toTop: true,
+//         cells: [
+//           {
+//             columnId: myColumnId,
+//             value: "Table Project",
+//           },
+//         ],
+//       },
+//     ];
+
+//     // Set options
+//     var options = {
+//       sheetId: sheetInfo.id,
+//       body: rows,
+//     };
+//     smartsheet.sheets
+//       .addRows(options)
+//       .then(function (newRows) {
+//         console.log(newRows);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   })
+  
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+    
 
