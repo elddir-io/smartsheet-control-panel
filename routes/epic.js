ad173799a176ac56f66dbd7d1e7548019722638e
Robert Riddle <robert@elddir.io>
2022-04-12 19:38:49 -0400

    add routes file, add some more styles, add getSheet and filter for columnId

---
+++ routes/epic.js
@@ -0,0 +1,32 @@
+const client = require('smartsheet');
+
+const arr1 = 
+[
+    'Connect data sources',
+    'Understand/edit fields',
+    'Custom calculations',
+    'Build visualizations',
+    'Build dashboard',
+    'Visualization Standars Approval', 
+    'Publish as Draft in Tableau staging server',
+    'Request list of Users that will be part of UAT/BAT',
+    'Review #1',
+    'Edits/Changes #1',
+    'Review #2',
+    'Edits/Changes #2',
+    'Review #3',
+    'Edits/Changes #3',
+    'UAT / BAT',
+    'Publish dashboard in Team\'s Tableau prod Server',
+    'Create Dashboard document',
+    'Send draft for approval',
+    'Publish Dashboard document in Doc Central (Word) / SP (PDF)',
+    'Develop Sharepoint Site',
+    'Upload the dashboard link and documentation to SharePoint',
+    'Office Hours week #1',
+    'Office Hours week #2',
+
+   ]
+
+   module.exports = createEpic;
+   
---
+++ routes/story.js
@@ -0,0 +1,127 @@
+const client = require('smartsheet');
+
+
+
+
+const smartsheet = client.createClient({
+    accessToken: process.env.ACCESS_TOKEN,
+    logLevel: "info",
+});
+
+
+const options = {
+    id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
+};
+
+
+function createStory (req, res) {
+    
+    const project = req.body.project;
+    const scope = req.body.scope;
+    const type = req.body.type;
+    const priority = req.body.priority;
+    const team = req.body.team;
+    const assigned = req.body.assigned;    
+    const targetDate = req.body.targetDate;
+    const email = req.body.email;
+    const name = req.body.name;
+
+    function target () {
+        let d = new Date(targetDate).toISOString();
+        return d;
+    }
+
+
+        // Get sheet
+    smartsheet.sheets
+    .getSheet(options)
+    .then(function (sheetInfo) {
+    const projName = sheetInfo.columns.find((c) => c.title === "Project Name");
+    const desScope = sheetInfo.columns.find((c) => c.title === "Project Description / Scope");
+    const issueType = sheetInfo.columns.find((c) => c.title === "Issue Type");
+    const priOrity = sheetInfo.columns.find((c) => c.title === "Priority");
+    const teams = sheetInfo.columns.find((c) => c.title === "Team");
+    const assign = sheetInfo.columns.find((c) => c.title === "Assigned to");    
+    const eta = sheetInfo.columns.find((c) => c.title === "Target Date");    
+    const eMail = sheetInfo.columns.find((c) => c.title === "Requestor Email");
+    const rname = sheetInfo.columns.find((c) => c.title === "Requestor Name");
+
+    const projNameId = projName.id;
+    const desScopeId = desScope.id;
+    const issueTypeId = issueType.id;
+    const priorityId = priOrity.id;
+    const teamsId = teams.id;
+    const assignId = assign.id;    
+    const etaId = eta.id;    
+    const emailId = eMail.id;
+    const nameId = rname.id;
+
+    // Specify rows
+    const rows = [
+        {
+        toTop: true,
+        cells: [
+            {
+            columnId: projNameId,
+            value: project,
+            },
+            {
+            columnId: desScopeId,
+            value: scope,
+            },
+            {
+            columnId: issueTypeId,
+            value: type,
+            },
+            {
+            columnId: priorityId,
+            value: priority,
+            },
+            {
+            columnId: teamsId,
+            value: team,
+            },
+            {
+            columnId: assignId,
+            value: assigned,
+            },            
+            {
+            columnId: etaId,
+            value: target(),
+            },            
+            {
+            columnId: emailId,
+            value: email,
+            },
+            {
+            columnId: nameId,
+            value: name,
+            },
+        ],
+        },
+    ];
+
+    // Set options
+    const options = {
+        sheetId: sheetInfo.id,
+        body: rows,
+    };
+    smartsheet.sheets
+        .addRows(options)
+        .then(function (newRows) {
+        console.log(newRows);
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    })
+
+    .catch(function (error) {
+    console.log(error);
+    });
+
+    res.redirect('/');
+};
+
+
+module.exports = createStory;
