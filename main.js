f9856279e5d59584013f3125098d3a782f143a1f
Robert Riddle <robert@elddir.io>
2022-05-13 16:38:31 -0400

    add ejs, split html into views, push analyst projects to browser, start html formating for analyst projects

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const ejs = require('ejs')
 require('dotenv').config();
 const app = express();
 
+// const { sheet } = require('smartsheet/lib/utils/constants');
 const story = require('./routes/story');
-const { sheet } = require('smartsheet/lib/utils/constants');
-// const epic = require('./epic')
+// const epic = require('./routes/epic')
 
 app.set("view engine", "ejs");
 
@@ -25,21 +25,7 @@ const smartsheet = client.createClient({
 const options = {
     id: process.env.SHEET_ID, // Id of Sheet <testing sheet>    
 };
-// THIS WORKS!! not ideal, but it works
-// sheetRows.forEach(function(value) {
-//     let rowData = value.cells[3].displayValue 
-    // if(rowData != undefined) {
-    //     console.log(rowData)
-    // }           
-    
-// });
 
-
-
-
-
-
-const teamRoster = ["Mickey", "Donald", "Goofy"]
 const team = {
     mickey: {
         projects: []
@@ -51,66 +37,46 @@ const team = {
         projects: []
     }
 }
-const projectList = []
 
-// function getAnalystProjects(req, res) {
+const mickey = team.mickey.projects
+const donald = team.donald.projects
+const goofy = team.goofy.projects
+
+// app.get('/', (req, res) => {
     // Get sheet, push data to array 
     smartsheet.sheets.getSheet(options)
     .then(function(sheetInfo) {
         const sheetRows = sheetInfo.rows        
         const projectColumnID = sheetInfo.columns.find((co) => co.title === "Project Name").id
         const assignedToID = sheetInfo.columns.find((co) => co.title === "Assigned to").id
-        // console.log(sheetRows)
-        
-        sheetRows.forEach(value => {
-            const cellData = value.cells
-            projectList.push(cellData)
-        });
-        
-        // console.log(projectList)
-        
-        
-        
-        // Mickey
-        // const mickey = cellData.filter(function(value) {
-
-        // })
-
-        // it's time to add ejs. need to push dynamic content
-        // https://levelup.gitconnected.com/render-dynamic-content-in-nodejs-using-templates-a58cae681148
-
+               
         // get projects for a given analyst
-        // sheetRows.forEach(function(value) {
-        //     const cellData = value.cells
-        //     const projectName = cellData.find((ce) => ce.columnId === projectColumnID).displayValue
-        //     const analystName = cellData.find((ce) => ce.columnId === assignedToID).displayValue
-        //     if(analystName === "Mickey") {team.mickey.projects.push(projectName)}  
-        //     if(analystName === "Donald") {team.donald.projects.push(projectName)}  
-        //     if(analystName === "Goofy") {team.goofy.projects.push(projectName)}       
-        // });
-        
-        // const mickey = team.mickey.projects
-        // const donald = team.donald.projects
-        // const goofy = team.goofy.projects
-        
-        // console.log(`Mickey's Projects:  ${mickey}`)
-        // console.log(`Donald's Projects:  ${donald}`)
-        // console.log(`Goofy's Projects:  ${goofy}`)
-            
-        
+        sheetRows.forEach(function(value) {            
+            const cellData = value.cells
+            const projectName = cellData.find((ce) => ce.columnId === projectColumnID).displayValue
+            const analystName = cellData.find((ce) => ce.columnId === assignedToID).displayValue
+            if(analystName === "Mickey") {team.mickey.projects.push(projectName)}  
+            if(analystName === "Donald") {team.donald.projects.push(projectName)}  
+            if(analystName === "Goofy") {team.goofy.projects.push(projectName)}       
+        }); 
     })
     .catch(function(error) {
         console.log(error)
     }); // close get sheet
     
-// };    
+    
+// });    
 // console.log(getAnalystProjects())
 
 /* ************************************************************************************ */
 
 
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render("index", 
+    {
+        mickeysProjects: mickey, 
+        donaldsProjects: donald
+    });
 });
 
 // create new rows in Smartsheet
