03a799b09f5f205eb999c39c2b16ced2eb27723f
Robert Riddle <robert@elddir.io>
2022-05-25 21:15:43 -0400

    move getAnalystProjects function to it's own module - working

--- main.js
+++ main.js
@@ -4,78 +4,19 @@ const ejs = require('ejs')
 require('dotenv').config();
 const app = express();
 
-// const { sheet } = require('smartsheet/lib/utils/constants');
 
-// const getAnalystProjects = require('./routes/analystProjects');
+
+const getAnalystProjects = require('./routes/analystProjects');
 const story = require('./routes/story');
 // const epic = require('./routes/epic')
 
+
 app.set("view engine", "ejs");
-
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 
-// get analyst projects
-// app.use(getAnalystProjects)
-
-/************************************************************************ */
-
-const smartsheet = client.createClient({
-    accessToken: process.env.ACCESS_TOKEN,
-    logLevel: "info",
-});
-
-
-const options = {
-    id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
-};
-
-const team = {
-    mickey: {
-        projects: []
-    },
-    donald: {
-        projects: []
-    },
-    goofy: {
-        projects: []
-    }
-};
-
-
-function getAnalystProjects() {
-    // Get sheet, push data to array 
-    smartsheet.sheets.getSheet(options)
-    .then(function(sheetInfo) {
-        const sheetRows = sheetInfo.rows        
-        const projectColumnID = sheetInfo.columns.find((co) => co.title === "Project Name").id
-        const assignedToID = sheetInfo.columns.find((co) => co.title === "Assigned to").id
-               
-        // get projects for a given analyst
-        sheetRows.forEach(function(value) {            
-            const cellData = value.cells
-            const projectName = cellData.find((ce) => ce.columnId === projectColumnID).displayValue
-            const analystName = cellData.find((ce) => ce.columnId === assignedToID).displayValue
-            if(analystName === "Mickey") {team.mickey.projects.push(projectName)}  
-            if(analystName === "Donald") {team.donald.projects.push(projectName)}  
-            if(analystName === "Goofy") {team.goofy.projects.push(projectName)}       
-        }); 
-    })
-    .catch(function(error) {
-        console.log(error)
-    }); // close get sheet
-};
-getAnalystProjects()
-
-const mickey = team.mickey.projects
-const donald = team.donald.projects
-const goofy = team.goofy.projects
-
-/************************************************************************ */
-   
-// app.use((req, res) => {})
+  
 
 app.get('/', (req, res) => {
     res.render("index", 
@@ -97,3 +38,5 @@ app.listen(3000, (req, res) => {
     console.log('server listening on port 3000 -- http://localhost:3000/')
 });   
 
+
+// app.use((req, res) => {})
--- routes/analystProjects.js
+++ routes/analystProjects.js
@@ -1,3 +1,4 @@
+
 const client = require('smartsheet');
 
 const smartsheet = client.createClient({
@@ -10,50 +11,51 @@ const options = {
     id: process.env.SHEET_ID, // Id of Sheet <testing sheet>
 };
 
-const team = {
-    mickey: {
-        projects: []
-    },
-    donald: {
-        projects: []
-    },
-    goofy: {
-        projects: []
-    }
-}
+// const team = {
+//     mickey: {
+//         projects: []
+//     },
+//     donald: {
+//         projects: []
+//     },
+//     goofy: {
+//         projects: []
+//     }
+// }
+
+mickey = []
+donald = []
+goofy = []
 
 
-
-function getAnalystProjects(req, res){
     // Get sheet, push data to array 
-    smartsheet.sheets.getSheet(options)
-    .then(function(sheetInfo) {
-        const sheetRows = sheetInfo.rows        
-        const projectColumnID = sheetInfo.columns.find((co) => co.title === "Project Name").id
-        const assignedToID = sheetInfo.columns.find((co) => co.title === "Assigned to").id
-               
-        // get projects for a given analyst
-        sheetRows.forEach(function(value) {            
-            const cellData = value.cells
-            const projectName = cellData.find((ce) => ce.columnId === projectColumnID).displayValue
-            const analystName = cellData.find((ce) => ce.columnId === assignedToID).displayValue
-            if(analystName === "Mickey") {team.mickey.projects.push(projectName)}  
-            if(analystName === "Donald") {team.donald.projects.push(projectName)}  
-            if(analystName === "Goofy") {team.goofy.projects.push(projectName)}       
-        }); 
-    })
-    .catch(function(error) {
-        console.log(error)
-    }); // close get sheet
-
-    const mickey = team.mickey.projects
-    const donald = team.donald.projects
-    const goofy = team.goofy.projects
-
-    // res.redirect('/')
-};
+exports.getAnalystProjects = smartsheet.sheets.getSheet(options)
+.then(function(sheetInfo) {
+    const sheetRows = sheetInfo.rows        
+    const projectColumnID = sheetInfo.columns.find((co) => co.title === "Project Name").id
+    const assignedToID = sheetInfo.columns.find((co) => co.title === "Assigned to").id
+            
+    // get projects for a given analyst
+    sheetRows.forEach(function(value) {            
+        const cellData = value.cells
+        const projectName = cellData.find((ce) => ce.columnId === projectColumnID).displayValue
+        const analystName = cellData.find((ce) => ce.columnId === assignedToID).displayValue
+        if(analystName === "Mickey") {mickey.push(projectName)}  
+        if(analystName === "Donald") {donald.push(projectName)}  
+        if(analystName === "Goofy") {goofy.push(projectName)}       
+    }); 
+})
+.catch(function(error) {
+    console.log(error)
+});
+    
+module.exports = mickey
+module.exports = donald
+module.exports = goofy
 
 
-// getAnalystProjects()
+// const mickey = team.mickey.projects
+// const donald = team.donald.projects
+// const goofy = team.goofy.projects
 
-// module.exports = getAnalystProjects
+ 
